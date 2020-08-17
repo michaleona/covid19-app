175f300d97577b90f3f350a61b8d49ae5d9d603f
Patricia <patricia@prixa.ai>
2020-08-17 15:43:21 +0700

    Second Commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/covid19-app/'
+    : '/',
   // ...other vue-cli plugin options...
   pwa: {
     name: 'Covid19 App',
@@ -11,8 +14,8 @@ module.exports = {
     // workboxPluginMode: 'InjectManifest',
     // workboxOptions: {
     //   // swSrc is required in InjectManifest mode.
-    //   swSrc: 'dev/sw.js',
+    //   swSrc: 'dist/service-worker.js',
     //   // ...other Workbox options...
     // }
-  }
-}
+  },
+};
